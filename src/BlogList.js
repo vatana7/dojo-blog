7dcca3737af502a280d69c4bf3c3abe0cab088d2
vatana7 <vatanamain@gmail.com>
2021-07-28 21:38:06 +0700

    rando push

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,14 @@
-function BlogList(props) {
-    const blogs = props.blogs;
-    const title = props.title;
+function BlogList({blogs, title, handleDelete}) {
 
     return (
         <div className="blog-list">
-            <h2>{title}</h2>
+            <h2>{ title }</h2>
         {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
                 <h2>{ blog.title }</h2>
                 <h4>{ blog.body }</h4>
-                <p>{ blog.author }</p>
+                <p>Written by { blog.author }</p>
+                <button onClick={() => handleDelete(blog.id)}>Delete article</button>
             </div>
         ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 function Home() {
+
+    const [name, setName] = useState('Mario')
 
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
     ])
+
+    useEffect(() => {
+        console.log('useEffect ran');
+        console.log(blogs)
+    }, [name]);
+
+    function handleDelete(id) {
+        const newBlog = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlog)
+    }
     return(
         <div className="home">
-            <BlogList blogs = {blogs} title = {"Hello World"}/>
+            <BlogList blogs = {blogs} title = {"All blogs"} handleDelete={handleDelete}/>
+            <button onClick={() => setName('Luigi')}>Change Name</button>
+            <p>{name}</p>
         </div>
     )
 }
